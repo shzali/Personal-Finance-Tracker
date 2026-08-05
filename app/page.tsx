1a7bcccdbccc5ff617c81f35b3fb3ebf65504685
ec23052 <ec23052@qmul.ac.uk>
2026-08-05 14:20:26 +0100

    initial commit: very basic investings and savings calculator after subtracting total expenses from income

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,93 @@
+// Extract to its own component
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
+
+interface ratio {
+  investings: number;
+  savings: number;
+}
 
 export default function Home() {
+  const [income, setIncome] = useState<number>(0);
+  const [expenses, setExpenses] = useState<number[]>([]);
+  const [ratio, setRatio] = useState<ratio>({ investings: 0.5, savings: 0.5 });
+  const [errorMsg, setErrorMsg] = useState<string>("");
+
+  const handleChange = (newVal: string, index: number) => {
+    if (parseInt(newVal)) {
+      const newExpenses = [...expenses];
+      newExpenses[index] = Number(newVal);
+      setExpenses(newExpenses);
+    }
+  };
+
+  // Return the sum of all expenses
+  const getTotalExpenses = () => {
+    return [...expenses].reduce((total, current) => total + current, 0);
+  };
+
+  const changeRatios = (newVal: string) => {
+    // if (parseInt(newVal)) {
+    setRatio({
+      investings: parseFloat(newVal),
+      savings: 100 - parseFloat(newVal),
+    });
+    // }
+  };
+
+  const getPercentages = () => {
+    setErrorMsg("");
+    const totalExpenses = getTotalExpenses();
+    if (totalExpenses > income) {
+      setErrorMsg("Income cannot be less than total expenses");
+    }
+  };
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <>
+      <p>Last month's income:</p>
+      <input
+        type="number"
+        value={income}
+        onChange={(e) => setIncome(parseFloat(e.target.value))}
+      />
+      <p>Expenses:</p>
+      <button onClick={() => setExpenses([...expenses, 0])}>+</button>
+      {expenses.map((val, index) => (
+        <input
+          type="number"
+          key={index}
+          value={val}
+          onChange={(e) => handleChange(e.target.value, index)}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+      ))}
+      <br />
+      <br />
+      <hr />
+      <p>Investings : Savings</p>
+      <input
+        type="number"
+        min="0"
+        max="100"
+        value={ratio.investings}
+        onChange={(e) => changeRatios(e.target.value)}
+      />{" "}
+      : <input type="number" min="0" max="1" value={ratio.savings} disabled />
+      <br />
+      <br />
+      <button onClick={getPercentages}>Generate</button>
+      {!errorMsg && (
+        <>
+          <p>
+            Invest: {((income - getTotalExpenses()) / 100) * ratio.investings}
           </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+          <br />
+          <p>Save: {((income - getTotalExpenses()) / 100) * ratio.savings}</p>
+        </>
+      )}
+      <p style={{ color: "red" }}>{errorMsg}</p>
+    </>
   );
 }
